c7a0f863ffeadb9a10f5adbb62b39fd77cb78468
rasikaitankar <rasikaitankar21105@gmail.com>
2026-07-15 23:06:48 +0530

    Implementation of mandatory hands-on for 1-spring-rest-handson is added in module 7

---
+++ Deepskilling/Module7_Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/Country.java
@@ -0,0 +1,33 @@
+package com.cognizant.spring_learn;
+
+public class Country {
+
+	private String code;
+	private String name;
+	public Country() {
+	}
+	public Country(String code, String name) {
+		super();
+		this.code = code;
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Country [code=" + code + ", name=" + name + "]";
+	}
+	
+	
+	
+}
---
+++ Deepskilling/Module7_Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,27 @@
+package com.cognizant.spring_learn;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+//		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		
+		displayCountry();
+//		SimpleDateFormat format = context.getBean("dateFormat",SimpleDateFormat.class);
+		
+	}
+	private static void displayCountry() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		
+		Country country = context.getBean("country",Country.class);
+		
+		System.out.println(country);
+		context.close();
+	}
+
+}
